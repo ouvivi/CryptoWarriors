bf70df9056a9fc5e4376dc8e03449a73c815b1c7
ouvivi <93237811+ouvivi@users.noreply.github.com>
2021-12-14 18:53:55 -0800

    Add files via upload

---
+++ precache-manifest.ef9eedcab27b3d66d96f285c64281508.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ca9ef6f041a3437fa89a38a1717bd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "069abfab5f9191619f99",
+    "url": "/static/css/main.9136e423.chunk.css"
+  },
+  {
+    "revision": "f67215b980f4ae22c826",
+    "url": "/static/js/2.54561ccc.chunk.js"
+  },
+  {
+    "revision": "45ca725996a86fc7b774ef915f1c0993",
+    "url": "/static/js/2.54561ccc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c6070c66c97f163c841",
+    "url": "/static/js/3.ee76a521.chunk.js"
+  },
+  {
+    "revision": "069abfab5f9191619f99",
+    "url": "/static/js/main.ac4a8719.chunk.js"
+  },
+  {
+    "revision": "aa194623e7f3315b810d",
+    "url": "/static/js/runtime-main.c1bb6951.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  }
+]);
